713999a6684809ca1194b2ec23f010e5f14c7b19
Chris Kimberley <ckimbe02@mail.bbk.ac.uk>
2016-02-13 18:59:13 +0000

    paired with Graeme

---
+++ ProgrammeRead10.java
@@ -0,0 +1,39 @@
+import com.sun.tools.doclets.internal.toolkit.util.SourceToHTMLConverter;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ * Created by graemewilkinson on 01/02/16.
+ */
+public class ProgrammeRead10 {
+
+    public static void main(String[] args) {
+        int total = 0;
+        int counter = 1;
+        int top = 0;
+        while (top == 0) {
+            try {
+                Scanner input = new Scanner(System.in);
+                System.out.println("Please enter number of numbers you'd like to enter");
+                top = input.nextInt();
+            } catch (InputMismatchException ex) {
+                System.out.println("That's not an int");
+            }
+        }
+        while (counter <= top) {
+            try {
+                Scanner input = new Scanner(System.in);
+                System.out.println("Please enter int number " + counter);
+                total = total + input.nextInt();
+                counter++;
+            } catch (InputMismatchException ex) {
+                System.out.println("That's not an int");
+            }
+        }
+
+        System.out.println("Average: " + total / top);
+    }
+
+
+}
